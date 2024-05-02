1bc914e7785839a6c5e2a8d387256aa7e480d393
TexasCoding <jeffw10278@proton.me>
2024-05-01 20:45:05 -0500

    changed main to run all only in 9AM timeframe. anything else only sells

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from alpaca_only_api.alpaca import *
 
+from datetime import datetime
+import pytz
+now = datetime.now(tz=pytz.timezone('US/Eastern'))
+print("now time is: ", now.strftime("%m/%d/%Y, %H:%M:%S"))
+current_hour = now.hour
+current_minute = now.minute
+
 def main():
     # Set the Alpaca API key and secret key
     alpaca = AlpacaAPI()
-
-    # Check for sell orders based on sell criteria
-    alpaca.sell_orders_from_sell_criteria()
-    # Check account for capital and liquidate positions if needed
-    alpaca.liquidate_positions_for_capital()
-    # Check for buy orders based on buy criteria
-    alpaca.buy_orders()
+    
+    if current_hour == 9 and current_minute < 50:
+        # Check for sell orders based on sell criteria
+        alpaca.sell_orders_from_sell_criteria()
+        # Check account for capital and liquidate positions if needed
+        alpaca.liquidate_positions_for_capital()
+        # Check for buy orders based on buy criteria
+        alpaca.buy_orders()
+    else:
+        # Check for sell orders based on sell criteria
+        alpaca.sell_orders_from_sell_criteria()
     
 if __name__ == "__main__":
     main()
